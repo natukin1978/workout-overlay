979abd157b51809b404432bf0618ff27802cb5f2
natukin1978 <natukin1978@hotmail.com>
2026-06-10 22:34:45 +0900

    set base.

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/workout-overlay/',
   server: {
     port: 9029,
     strictPort: true,
